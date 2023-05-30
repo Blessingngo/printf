2ea89d6122509049f69f1902bc79abee866da55a
Blessing Victor <blessingvictor998@gmail.com>
2023-05-30 16:30:04 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,61 +1,49 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Number of Printed characters.
- */
+#include <stdarg.h>
+#include <stdio.h>
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, int width, int precision, int size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_list args;
+	int count = 0;
 
 	if (format == NULL)
 	return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	va_start(args, format);
+	while (*format)
 {
-	if (format[i] != '%')
+	if (*format == '%')
 {
-	buffer[buff_ind++] = format[i];
-	if (buff_ind == BUFF_SIZE)
-	print_buffer(buffer, &buff_ind);
-	printed_chars++;
+	format++;
+	switch (*format)
+{
+	case 'c':
+	putchar(va_arg(args, int));
+	count++;
+	break;
+	case 's':
+{
+	const char *str = va_arg(args, const char *);
+
+	while (*str)
+{
+	putchar(*str++);
+	count++;
+}
+	break;
+}
+	case '%':
+	putchar('%');
+	count++;
+	break;
+}
 }
 	else
 {
-	print_buffer(buffer, &buff_ind);
-	flags = get_flags(format, &i);
-	width = get_width(format, &i, list);
-	precision = get_precision(format, &i, list);
-	size = get_size(format, &i);
-	++i;
-	printed = handle_print(format, &i, list, buffer,
-	flags, width, precision, size);
-	if (printed == -1)
-	return (-1);
-	printed_chars += printed;
+	putchar(*format);
+	count++;
 }
+	format++;
 }
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
-}
-
-/**
- * print_buffer - function that prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next character, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-	write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
+	va_end(args);
+	return (count);
 }
